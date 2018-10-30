6e3c99d7f5f4b56b33662b5cd7732c37aa0b2c37
santirampoldi <santirampoldi12@gmail.com>
2018-10-30 01:24:44 -0300

    Avances sobre presentacion, movimiento del personaje y refactor

--- Entrega4/js/game.js
+++ Entrega4/js/game.js
@@ -10,18 +10,14 @@ class Game {
   constructor(args = {}) {
     this.spaceship = new gameObject({
       player: "spaceship",
-      w: 55,
-      h: 95,
+      w: 70,
+      h: 90,
       num: 0,
       htmlDOM: '<div id="spaceship" class="spaceship"></div>'
     });
-    this.enemyQueue = [];
+    this.enemiesQueue = [];
     this.spawnedEnemiesQueue = [];
-    // this.music = new Audio("audio/bgMusic.mp3");
-    // this.music.volume = 0.5;
-    // this.music.loop = true;
-    // this.width = 640;
-    // this.height = 530;
+    this.factorMovimiento = 8;
     this.keyCodes = {
       left: 37,
       up: 38,
@@ -30,42 +26,192 @@ class Game {
     }
     this.keys = [];
     this.start = false;
-    this.enemyCount = 0;
     this.intervals = [];
     this.score = 0;
-    this.setControlsEvents();
+    this.setControls();
     this.level = 0;
   }
 
-  setControlsEvents() {
+  setControls() {
     let thisClass = this;
 
     document.addEventListener("keydown", function (e) {
-      console.log(e.keyCode);
       if (thisClass.start) {
         thisClass.keys[e.keyCode] = true;
       }
     });
 
     document.addEventListener("keyup", function (e) {
-      console.log(e.keyCode);
       if (thisClass.start) {
         thisClass.keys[e.keyCode] = false;
       }
     });
-
-    // document.getElementById("down").addEventListener("click", () => {
-    //   this.setVolume("down");
-    // });
-    //
-    // document.getElementById("up").addEventListener("click", () => {
-    //   this.setVolume("up");
-    // });
   }
+
+  startGame() {
+    this.clear();
+    document.getElementById("game").classList.add("gameStarted");
+    this.start = true;
+    this.spawnPlayer();
+    this.spawnEnemy();
+    this.startFlight();
+
+    document.getElementById("spaceship").addEventListener("animationend", () => {
+      let elem = document.getElementById("spaceship").classList;
+      if (elem.contains("explosion")) {
+        this.cleanGame();
+        let perdiste = "<h1 id='gameOver' class='gameOver'>Game Over!!</h1>";
+        $("#game").append(perdiste);
+      }
+    });
+  }
+
+  spawnEnemy() {
+
+  }
+
+  clear() {
+    let gameOver = document.getElementById("gameOver");
+    if (gameOver) {
+      gameOver.remove();
+    }
+    this.start = false;
+    this.keys = [];
+    this.score = 0;
+    document.getElementById("score").innerHTML = 0;
+    document.getElementById("game").classList.remove("gameStarted");
+
+    $("#spaceship").remove();
+    $(".enemy").remove();
+
+    this.spaceship = new gameObject({
+      player: "spaceship",
+      w: 60,
+      h: 90,
+      num: 0,
+      htmlDOM: '<div id="spaceship" class="spaceship"></div>'
+    });
+
+    for (let i = 0; i < this.enemiesQueue.length; i++) {
+      let aux = this.enemiesQueue.remove();
+      aux.animatedEnd = true;
+      clearInterval(aux.interval[0]);
+    }
+
+    for (let i = 0; i < this.spawnedEnemiesQueue.length; i++) {
+      let aux = this.spawnedEnemiesQueue.remove();
+      aux.animatedEnd = true;
+      clearInterval(aux.interval[0]);
+    }
+
+    this.enemiesQueue = [];
+    this.spawnedEnemiesQueue = [];
+
+    if (this.intervals.length > 0) {
+      for (let i = 0; i < this.intervals.length; i++) {
+        clearInterval(this.intervals[i]);
+      }
+    }
+    this.intervals = [];
+  }
+
+  detectCollision() {
+
+  }
+
+  gameOver() {
+    if (this.start) {
+      let gameOver = false;;
+      if (document.getElementById("spaceship") && !this.spawnedEnemiesQueue.length == 0) {
+        gameOver = this.detectCollision();
+      }
+
+      if (gameOver) {
+        this.spaceship.setAnimation("explosion");
+        this.calculateHighScore();
+      }
+    }
+  }
+
+  spawnPlayer() {
+    if (this.start) {
+      let elem = document.getElementById("game");
+      elem.innerHTML += this.spaceship.element;
+      this.spaceship.setPos("250px", "40px");
+    }
+  }
+
+  generateEnemy() {
+    let enemy = new gameObject({
+      player: "enemy",
+      w: 98,
+      h: 100,
+      num: n,
+      htmlDOM: '<div class="enemy" id="enemy' + n + '"></div>'
+    });
+    return enemy;
+  }
+
+  startFlight() {
+    let thisClass = this;
+
+    this.intervals[1] = setInterval(function () {
+      let elem = document.getElementById("spaceship").style;
+      let posX = parseInt(elem.left, 10);
+      let posY = parseInt(elem.bottom, 10);
+
+      if (thisClass.keys[thisClass.keyCodes.left]) {
+        thisClass.moveLeft(elem, posX, thisClass.factorMovimiento);
+      }
+      else if (thisClass.keys[thisClass.keyCodes.right]) {
+        thisClass.moveRight(elem, posX, thisClass.factorMovimiento);
+      }
+
+      if (thisClass.keys[thisClass.keyCodes.up]) {
+        thisClass.moveUp(elem, posY, thisClass.factorMovimiento);
+      }
+      else if (thisClass.keys[thisClass.keyCodes.down]) {
+        thisClass.moveDown(elem, posY, thisClass.factorMovimiento);
+      }
+
+      thisClass.gameOver();
+
+      if (!thisClass.start) {
+        clearInterval(this.intervals[1]);
+      }
+    }, 20);
+  }
+
+  moveLeft(elem, posX, factorMovimiento) {
+    if (posX > 8) {
+      posX -= factorMovimiento;
+      elem.left = posX + "px";
+    }
+  }
+
+  moveRight(elem, posX, factorMovimiento) {
+    if (posX < 640 - this.spaceship.width - 16) {
+      posX += factorMovimiento;
+      elem.left = posX + "px";
+    }
+  }
+
+  moveUp(elem, posY, factorMovimiento) {
+    if (posY < 530 - this.spaceship.height - 8) {
+      posY += factorMovimiento;
+      elem.bottom = posY + "px";
+    }
+  }
+
+  moveDown(elem, posY, factorMovimiento) {
+    if (posY > 5) {
+      posY -= factorMovimiento;
+      elem.bottom = posY + "px";
+    }
+  }
+
 }
 
-// SetTimeOut(Update, 1000/25);
-// var id = SetInterval(Update, 1000/25);
 // Update() {
 // Ver input y actualizar posicion
 // Actualizar obstaculos con animacion o con top+1
@@ -75,10 +221,4 @@ class Game {
 //  SetTimeOut(Update, 1000/25)
 // }
 // }
-// gameOver() {
-//  clearInterval(id);
-// }
-//
 // Llamar al update de todos los gameObject
-// Objetos de clase gameObject
-//
--- Entrega4/js/gameObject.js
+++ Entrega4/js/gameObject.js
@@ -1,1 +1,62 @@
 "use strict";
+
+class gameObject {
+  constructor(args = {}) {
+    this.player = args.player;
+    this.width = args.w;
+    this.height = args.h;
+    this.element = args.htmlDOM;
+    this.id = args.num;
+    this.top = 0;
+    this.bottom = 0;
+    this.left = 0;
+    this.right = 0;
+    this.animatedEnd = false;
+    this.interval = [];
+  }
+
+  calculatePos() {
+    let rect;
+    if (this.player == "spaceship") {
+      rect = document.getElementById("spaceship").getBoundingClientRect();
+    }
+    else if(this.player == "enemy") {
+      let id = "enemy" + this.id;
+      let elem = document.getElementById(id);
+      if(elem) {
+        rect = elem.getBoundingClientRect();
+      }
+    }
+
+    if (rect) {
+      this.top = rect.top;
+      this.bottom = rect.bottom;
+      this.left = rect.left;
+      this.right = rect.right;
+    }
+  }
+
+  setPos(x, y) {
+    let css = document.getElementById(this.player).style;
+    css.left = x;
+    css.bottom = y;
+  }
+
+  setPosEnemy(x, y) {
+    let id = this.player + this.id;
+    let css = document.getElementById(id).style;
+    css.left = x;
+    css.bottom = y;
+  }
+
+  setAnimation(animation) {
+    let elem = document.getElementById(this.player);
+    elem.classList.add(animation);
+  }
+
+  setAnimationEnemy(animation) {
+    let id = "enemy" + this.id;
+    let elem = document.getElementById(id);
+    elem.classList.add(animation);
+  }
+}
